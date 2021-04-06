0749b463d47e406162f8a6d1b92d2873351b43c4
vivian1008 <56061904+vivian1008@users.noreply.github.com>
2021-04-07 00:27:20 +0800

    Add files via upload

--- hash2.py
+++ hash2.py
@@ -12,5 +12,13 @@ with open('passwordList.txt' , 'r') as filehandle:
 
 if len(sys.argv) == 2:
     msg = sys.argv[1].encode()
-    hashValue = hashlib.sha256(msg)
-    print(hashValue.hexdigest().upper())
+    hashValue256 = hashlib.sha256(msg)
+    hashValue512 = hashlib.sha512(msg)
+    hashValue1 = hashlib.sha1(msg)
+    hashValueMD5 = hashlib.md5(msg)
+    print('msg:',msg)
+    print('sha256:',hashValue256.hexdigest().upper())
+    print('sha512:',hashValue512.hexdigest().upper())
+    print('sha1:',hashValue1.hexdigest().upper())
+    print('md5:',hashValueMD5.hexdigest().upper())
+    
